339a34602bd665b11ec35cce27be8c6a5c4c29c7
HaskMoney <heartstring85@gmail.com>
2022-07-13 17:52:14 -0700

    added details of books

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+HELPFUL TIPS:
+- store books objects in an array
+- write function that loops through arrays, 
+    display on screen
+- make book button functional
+- add button to remove book display
+- add button to change read status
+*/
+
+
